ee8973961aec69f4ac2581029c73286a5afb8179
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-10-09 01:33:51 +0200

    created seperate folder for back-end

---
+++ controllers/chatRouter.js
@@ -0,0 +1,88 @@
+const chatRouter = require('express').Router()
+const prisma = require('../models/prismaClient')
+const logger = require('../utils/logger')
+
+chatRouter.get('/', async (req, res) => {
+  const { senderId, receiverId } = req.query
+  const user = req.user
+
+  if (senderId !== user.id) return res.status(401).json({ error: 'Not Authorized' })
+
+  try {
+    const messages = await prisma.messages.findMany({
+      where: {
+        OR: [
+          { senderId: senderId, receiverId: receiverId },
+          { senderId: receiverId, receiverId: senderId }
+        ]
+      },
+      orderBy: {
+        createdAt: 'asc'
+      }
+    })
+    res.json(messages)
+  } catch (e) {
+    logger.error(e)
+    res.status(500).json({ error: 'An error occurred while retrieving messages.' })
+  }
+})
+
+
+chatRouter.post('/', async (req, res) => {
+  const { receiverId, senderId, content } = req.body
+
+  const receiver = await prisma.users.findUnique({
+    where: {
+      id: receiverId
+    }
+  })
+  const user = req.user
+
+  if (senderId !== user.id) return res.status(401).json({ error: 'Not Authorized' })
+  if (!receiver) return res.status(404).json({ error: 'invalid message receiver' })
+
+  try{
+    const newMsg = await prisma.messages.create({
+      data:{
+        content,
+        senderId,
+        receiverId,
+      }
+    })
+    res.status(201).json(newMsg)
+  }catch(e) {
+    logger.error(e)
+    res.status(500).json({ error: 'An error occurred while sending message.' })
+  }
+})
+
+// chatRouter.put('/', async (req, res) => {
+//     const { content } = req.body
+
+//     const receiver
+
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = chatRouter
---
+++ prisma/migrations/20241008201905_init/migration.sql
@@ -0,0 +1,68 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "name" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Groups" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "creatorId" TEXT NOT NULL,
+
+    CONSTRAINT "Groups_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "GroupMembers" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "groupId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "admin" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "GroupMembers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Messages" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "senderId" TEXT NOT NULL,
+    "receiverId" TEXT NOT NULL,
+    "groupId" TEXT,
+
+    CONSTRAINT "Messages_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_username_key" ON "Users"("username");
+
+-- AddForeignKey
+ALTER TABLE "Groups" ADD CONSTRAINT "Groups_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "GroupMembers" ADD CONSTRAINT "GroupMembers_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "GroupMembers" ADD CONSTRAINT "GroupMembers_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Groups"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Messages" ADD CONSTRAINT "Messages_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Messages" ADD CONSTRAINT "Messages_receiverId_fkey" FOREIGN KEY ("receiverId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Messages" ADD CONSTRAINT "Messages_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Groups"("id") ON DELETE SET NULL ON UPDATE CASCADE;
